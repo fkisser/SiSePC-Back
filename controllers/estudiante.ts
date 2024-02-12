d5674f560b737eb6315ac1188526becf6f846eda
Facundo <facundo.kisser@gmail.com>
2024-02-12 12:42:05 -0300

    el estudiante se cre acon el detalle de las asignaturas

--- controllers/estudiante.ts
+++ controllers/estudiante.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Estudiante, { IEstudiante } from "../models/estudiante";
+import Plan, { IPlan } from "../models/plan";
 
 export const getEstudiantes = async (req: Request, res: Response) => {
 	const estudiantes: IEstudiante[] = await Estudiante.find()
@@ -40,7 +41,7 @@ export const getEstudianteByDNI = async (req: Request, res: Response) => {
 
 export const createEstudiante = async (req: Request, res: Response) => {
 	const estudianteData = req.body;
-	const { apellido, nombre, dni, mail, plan } = estudianteData;
+	const { apellido, nombre, dni, mail, plan }: IEstudiante = estudianteData;
 	if (!apellido || !nombre || !dni || !mail || !plan) {
 		res.status(400).json({
 			msj: "El estudiante debe tener al menos: nombre, apellido, dni, mail y un plan asignados",
@@ -57,7 +58,11 @@ export const createEstudiante = async (req: Request, res: Response) => {
 		});
 		return;
 	}
-	const estudiante = new Estudiante(estudianteData);
+	const _plan: IPlan | null = await Plan.findById(plan);
+	const detallePlan = _plan?.asignaturas?.map((asignatura) => {
+		return { asignaturaOriginal: asignatura.nombre, condicion: "Pendiente" };
+	});
+	const estudiante = new Estudiante({ ...estudianteData, detallePlan });
 	await estudiante.save();
 	res.status(201).json({
 		msg: "Estudiante creado con éxito",
