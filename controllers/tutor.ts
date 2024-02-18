f3432774088809e548c27c5a64664b0e8403e705
Facundo <facundo.kisser@gmail.com>
2024-02-18 10:50:40 -0300

    cambio en la manera de obtener tutores

--- controllers/tutor.ts
+++ controllers/tutor.ts
@@ -4,9 +4,22 @@ import Tutor, { ITutor } from "../models/tutor";
 export const getTutores = async (req: Request, res: Response) => {
 	const tutores: ITutor[] = await Tutor.find();
 	const partTutores = tutores.map((tutor) => {
-		const { apellido, nombre, dni, mail, celular, estudiantes } = tutor;
-		return { apellido, nombre, dni, mail, celular, estudiantes };
+		const { contraseña, isAdmin, visible, recuperacion } = tutor;
+		return { ...tutor };
 	});
 	res.status(200).json({ partTutores });
+	return;
+};
+export const getIDbyDNI = async (req: Request, res: Response) => {
+	// const { DNI } = req.params;
+	// const tutor: ITutor | null = await Tutor.findOne({ dni: DNI });
+	// if (tutor) {
+	// 	tutor.res.status(200).json({});
+	// }
+	// const partTutor = tutores.map((tutor) => {
+	// 	const { contraseña, isAdmin, visible, recuperacion } = tutor;
+	// 	return { ...tutor };
+	// });
+
 	return;
 };
